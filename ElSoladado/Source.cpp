3f3d9a01420ec574ae5e1bb8d016ca78673aeeec
Gonzalo <gonzaweiss@gmail.com>
2023-04-16 23:18:00 -0300

    Menú completo

--- ElSoladado/Source.cpp
+++ ElSoladado/Source.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 
 int main() {
+	int opcion;
 	cout << "-== Bienvenido al campo de entrenamiento, Soldado ==-" << endl <<endl << endl;
-	cout << "¿Qué desea hacer?"<<endl;
+	cout << "¿Qué desea hacer?" << endl << endl;
+	cout << "1- Recoger arma"<<endl;
+	cout << "2- Dejar arma" << endl;
+	cout << "3- Ver arma en uso" << endl;
+	cout << "X- Salir" << endl;
+	cin >> opcion;
+	if (opcion == 1) {
+		system("CLS");
+			cout << "Por favor recoja el arma que desee utilizar:" << endl <<endl;
+			cout << "1- Revolver" << endl;
+			cout << "2- Rifle" << endl;
+			cout << "3- Escopeta" << endl;
+			cout << "X- Volver atrás" << endl
+	}
+	
+	
+	;
+
 	return 0;
 }
